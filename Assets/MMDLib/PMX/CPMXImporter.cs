ebdaf395434d6a6bd9811efc9dd2337703d7581a
haruoogawa <haru.spring.yodai@gmail.com>
2025-11-01 12:58:23 +0900

    マテリアルロードを実装

--- Assets/MMDLib/PMX/CPMXImporter.cs
+++ Assets/MMDLib/PMX/CPMXImporter.cs
@@ -4,6 +4,7 @@ using System.IO;
 using UnityEditor;
 using UnityEditor.AssetImporters;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 // ScriptedImporter
 // https://docs.unity3d.com/6000.2/Documentation/ScriptReference/AssetImporters.ScriptedImporter.html
@@ -116,13 +117,13 @@ namespace mmdlib
 
         static bool Import(string fileName, string outDir, Dictionary<string, string> FolderMap)
         {
+            string srcFolder = Path.GetDirectoryName(fileName);
+
             CPmxModel model = new CPmxModel();
             if (!model.Analyse(fileName)) return false;
 
             List<GameObject> NodeList = new List<GameObject>();
-            List<Material> MaterialList = new List<Material>();
             List<Mesh> MeshList = new List<Mesh>();
-            List<Texture> TextureList = new List<Texture>();
 
             // ルートノードを生成
             string rootName = Path.GetFileNameWithoutExtension(fileName);
@@ -132,10 +133,13 @@ namespace mmdlib
             // Skeleton
             if (!CreateAnimationSkeleton(model, ref rootNode, ref NodeList)) return false;
 
-            // マテリアルリスト
-            if (!CreateMaterialList(model, ref NodeList, ref MaterialList)) return false;
-
             // テクスチャリスト
+            List<Texture> TextureList = new List<Texture>();
+            //if (!CreateTextureList(model, srcFolder, ref TextureList, FolderMap)) return false;
+
+            // マテリアルリスト
+            Dictionary<string, Material> MaterialMap = new Dictionary<string, Material>();
+            if (!CreateMaterialList(model, ref NodeList, ref MaterialMap, TextureList, FolderMap)) return false;
 
             // メッシュ
 
@@ -216,13 +220,121 @@ namespace mmdlib
             return true;
         }
 
-        static bool CreateMaterialList(CPmxModel model, ref List<GameObject> NodeList, ref List<Material> MaterialList)
+        static bool CreateTextureList(CPmxModel model, string srcFolder, ref List<Texture> TextureList,
+            Dictionary<string, string> FolderMap)
         {
-            foreach(var pmxMaterial in model.GetPmxMaterialList())
-            {
-                if (pmxMaterial == null) continue;
+            string TextureFolder = string.Empty;
+            if (!FolderMap.TryGetValue("Textures", out TextureFolder)) return false;
 
-                //pmxMaterial.GetMaterialName()
+            foreach(var PmxTexture in model.GetPmxTextureList())
+            {
+                string TexturePath = Path.Combine(srcFolder, PmxTexture.GetFilePath());
+
+                string fileName = Path.GetFileName(TexturePath);
+                string TexAssetName = Path.Combine(TextureFolder, fileName);
+
+                bool result = AssetDatabase.CopyAsset(TexturePath, TexAssetName);
+
+                Debug.LogFormat("TexturePath: {0}, TexAssetName: {1}, result: {2}", TexturePath, TexAssetName, result);
+
+                /*Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(TexturePath);
+                Debug.LogFormat("texture.name: {0}", texture.name);
+                if (texture == null)
+                {
+                    Debug.LogErrorFormat("Failed to load Texture. TexturePath: {0}", TexturePath);
+
+                    return false;
+                }
+
+                // アセットを保存
+                string fileName = Path.GetFileName(TexturePath);
+
+                string TexAssetName = Path.Combine(TextureFolder, fileName);
+                AssetDatabase.CreateAsset(texture, TexAssetName);
+                
+                TextureList.Add(texture); 
+                */
+
+            }
+
+            return true;
+        }
+
+        static bool CreateMaterialList(CPmxModel model, ref List<GameObject> NodeList, ref Dictionary<string, Material> MaterialMap,
+            List<Texture> TextureList, Dictionary<string, string> FolderMap)
+        {
+            string MaterialFolder = string.Empty;
+            if (!FolderMap.TryGetValue("Materials", out MaterialFolder)) return false;
+
+            foreach(var PmxMaterial in model.GetPmxMaterialList())
+            {
+                if (PmxMaterial == null) continue;
+
+                string MaterialName = PmxMaterial.GetMaterialName();
+
+                Material material = new Material(Shader.Find("MMDLib/BasicToon"));
+
+                // カリング
+                // https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Rendering.CullMode.html
+                CullMode cullMode = CullMode.Back;
+                if (PmxMaterial.IsDrawDoubleSlided()) cullMode = CullMode.Off;
+
+                material.SetInteger("_Cull", (int)cullMode);
+
+                // ブレンドモード
+                BlendMode blendSrc = BlendMode.SrcAlpha;
+                material.SetInteger("_BlendSrc", (int)blendSrc);
+
+                BlendMode blendDst = BlendMode.OneMinusSrcAlpha;
+                material.SetInteger("_BlendDst", (int)blendDst);
+
+                // ShaderUniformをセット
+                material.SetFloat("_EdgeSize", PmxMaterial.GetEdgeSize());
+                material.SetFloat("_SpecularIntensity", PmxMaterial.GetSpecularCoef());
+
+                material.SetColor("_DiffuseFactor", PmxMaterial.GetDiffuse());
+                material.SetColor("_AmbientFactor", PmxMaterial.GetAmbient());
+                material.SetColor("_SpecularFactor", PmxMaterial.GetSpecular());
+                material.SetColor("_EdgeColor", PmxMaterial.GetEdgeColor());
+
+                // MainTexture
+                int MainTexIndex = PmxMaterial.GetMainTexIndex();
+                if(MainTexIndex >= 0 && MainTexIndex < TextureList.Count)
+                {
+                    Texture texture = TextureList[MainTexIndex];
+                    material.SetTexture("_MainTexture", texture);
+                }
+
+                // ToonTexture
+                int ToonTexIndex = PmxMaterial.GetToonTexIndex();
+                int SharedToonTexIndex = PmxMaterial.GetSharedToonTexIndex();
+
+                if (ToonTexIndex >= 0 && ToonTexIndex < TextureList.Count)
+                {
+                    Texture texture = TextureList[ToonTexIndex];
+                    material.SetTexture("_ToonTexture", texture);
+                }
+                else if(SharedToonTexIndex >= 0)
+                {
+                    // 未対応
+                }
+
+                // SphereTexture
+                int SphereTexIndex = PmxMaterial.GetSphereTexIndex();
+                if (SphereTexIndex >= 0 && SphereTexIndex < TextureList.Count)
+                {
+                    Texture texture = TextureList[SphereTexIndex];
+                    material.SetTexture("_SphereTexture", texture);
+                }
+
+                //
+                MaterialMap.Add(MaterialName, material);
+
+                // マテリアルアセット生成
+                string MaterialAssetName = Path.Combine(MaterialFolder, MaterialName);
+                MaterialAssetName += ".mat";
+
+                AssetDatabase.CreateAsset(material, MaterialAssetName);
             }
 
             return true;
--- Assets/MMDLib/Shader/BasicToon.shader
+++ Assets/MMDLib/Shader/BasicToon.shader
@@ -1,17 +1,37 @@
-Shader "Custom/BasicToon"
+Shader "MMDLib/BasicToon"
 {
     Properties
     {
-        [MainColor] _BaseColor("Base Color", Color) = (1, 1, 1, 1)
-        [MainTexture] _BaseMap("Base Map", 2D) = "white"
+        _DiffuseFactor("DiffuseFactor", Color) = (1.0, 1.0, 1.0, 1.0)
+        _AmbientFactor("AmbientFactor", Color) = (1.0, 1.0, 1.0, 1.0)
+        _SpecularFactor("SpecularFactor", Color) = (1.0, 1.0, 1.0, 1.0)
+        _EdgeColor("EdgeColor", Color) = (1.0, 1.0, 1.0, 1.0)
+
+        _EdgeSize("EdgeSize", Float) = 1.0
+        _SpecularIntensity("SpecularIntensity", Float) = 1.0
+
+        _SphereMode("SphereMode", Int) = 0
+
+        _MainTexture("MainTexture", 2D) = "white"{}
+        _ToonTexture("ToonTexture", 2D) = "white"{}
+        _SphereTexture("SphereTexture", 2D) = "white"{}
+
+        [Enum(UnityEngine.Rendering.CullMode)] _Cull("Cull", Integer) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)] _BlendSrc("BlendSrc", Integer) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)] _BlendDst("BlendDst", Integer) = 1
     }
 
     SubShader
     {
-        Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" }
+        Tags { "RenderType" = "Opaque"}
+
+        Cull [_Cull]
+        Blend [_BlendSrc] [_BlendDst]
 
         Pass
         {
+            Tags{ "LightMode" = "SRPDefaultUnlit" }
+
             HLSLPROGRAM
 
             #pragma vertex vert
@@ -31,25 +51,30 @@ Shader "Custom/BasicToon"
                 float2 uv : TEXCOORD0;
             };
 
-            TEXTURE2D(_BaseMap);
-            SAMPLER(sampler_BaseMap);
-
-            CBUFFER_START(UnityPerMaterial)
-                half4 _BaseColor;
-                float4 _BaseMap_ST;
-            CBUFFER_END
+            float _EdgeSize;
 
             Varyings vert(Attributes IN)
             {
                 Varyings OUT;
                 OUT.positionHCS = TransformObjectToHClip(IN.positionOS.xyz);
-                OUT.uv = TRANSFORM_TEX(IN.uv, _BaseMap);
+                OUT.uv = IN.uv;
                 return OUT;
             }
 
+            float4 _DiffuseFactor;
+            float4 _AmbientFactor;
+            float4 _SpecularFactor;
+            float4 _EdgeColor;
+            float _SpecularIntensity;
+            int _SphereMode;
+
+            sampler2D _MainTexture;
+            sampler2D _ToonTexture;
+            sampler2D _SphereTexture;
+
             half4 frag(Varyings IN) : SV_Target
             {
-                half4 color = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, IN.uv) * _BaseColor;
+                float4 color = float4(1.0, 1.0, 1.0, 1.0);
                 return color;
             }
             ENDHLSL
