dc3ad6efe8a5a40eebc9289a242e4ca63aad85df
HaruoOgawa <haru.spring.yodai@gmail.com>
2025-12-19 08:19:48 +0900

    DMX受信イベントを購読できるように

--- Assets/Network/DMX/DMXDataHandler.cs
+++ Assets/Network/DMX/DMXDataHandler.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class DMXDataHandler : MonoBehaviour
+namespace network.dmx
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class DMXDataHandler : MonoBehaviour
     {
-        
-    }
+        public DMXDataHandler()
+        {
+            // イベント購読
+            UDPSocket.OnReceivedDMX += OnReceivedDMX;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnDestroy()
+        {
+            // イベント購読解除
+            UDPSocket.OnReceivedDMX -= OnReceivedDMX;
+        }
+
+        void OnReceivedDMX(ushort Net, ushort SubNet, ushort Universe, byte[] DataBuffer)
+        {
+            Debug.LogFormat("[DMXDataHandler.OnReceivedDMX] Net: {0}, SubNet: {1}, Universe: {2}, DataBuffer.Length: {3}", 
+                Net, SubNet, Universe, DataBuffer.Length);
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
     }
 }
